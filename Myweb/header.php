a8baf8c1733c194a0ca38ba90b799e80e09ca127
Vikas <42447141+babuhacker@users.noreply.github.com>
2019-04-08 11:11:17 -0500

    Add files via upload

---
+++ Myweb/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		
+	</head>
+	<body>
+		<div class="header">
+			<div class="navigation">
+				<nav class="navigation_bar">
+					<a href="#">Home</a>
+					<a href="#">Portfolio</a>
+					<a href="#">Projects</a>
+				</nav>
+			</div>
+			<div class="icons">
+					
+					<a href="#"><i class="fa fa-github" aria-hidden="true" id="icon1"></i></a>
+					
+					<a href="#"><i class="fa fa-github" aria-hidden="true" id="icon1"></i></a>
+					
+					<a href="#"><i class="fa fa-github" aria-hidden="true" id="icon1"></i></a>
+					
+					<a href="#"><i class="fa fa-github" aria-hidden="true" id="icon1"></i></a>
+					
+			</div>
+		</div>
+	</body>
+</html>
---
+++ Myweb/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/home.css">
+		<script>
+		
+			
+		</script>
+	</head>
+	<body>
+		
+		<div class="bug_song">
+		
+					RO<a href="#"><i class="fa fa-bug" aria-hidden="true" id="icon2"></i></a>T BABU
+					
+					
+					
+			
+		</div>
+		
+	</body>
+</html>
